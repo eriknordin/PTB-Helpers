7746f412a68c41d1063f39ebe96fd359b2efaf37
Erik Nordin <=>
2011-05-16 18:14:42 +0200

    Added Create PTB Classes

---
+++ CreatePTBClasses/Plugins/Admin/CreatePTBClasses.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreatePTBClasses.aspx.cs" Inherits="Antecknat.Web.CMS.Plugins.Admin.CreatePTBClasses" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+    <style>
+        body { font-family: Verdana; font-size: 11px; }
+        h3 { padding: 0; margin: 10px 0 3px; }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Create PageTypeBuilder classes from a current project</h1>
+        <asp:PlaceHolder ID="PlaceHolderInput" runat="server">
+            <div>
+                <h3>Prefix before class name:</h3></h3>
+                <asp:TextBox ID="TextBoxPrefixBefore" runat="server"></asp:TextBox>
+            </div>
+            
+            <div>
+                <h3>Prefix after class name:</h3>
+                <asp:TextBox ID="TextBoxPrefixAfter" runat="server">PageType</asp:TextBox>
+            </div>
+            
+            <div>
+                <h3>PageType base class:</h3>
+                <asp:TextBox ID="TextBoxExtend" runat="server">TypedPageData</asp:TextBox>
+            </div>
+            
+            <div>
+                <h3>Namespace:</h3>
+                <asp:TextBox ID="TextBoxNamespace" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="ReqFielsNamespace" ControlToValidate="TextBoxNamespace" runat="server"></asp:RequiredFieldValidator>
+            </div>
+            
+            <div>
+                <h3>Output directory (relative to Server.MapPath):</h3>
+                <asp:TextBox ID="TextBoxOutput" runat="server">PageTypes</asp:TextBox>
+            </div>
+            
+            <div>
+                <h3>Exclude following page types:</h3>
+                <asp:CheckBoxList ID="CheckBoxListPageTypes" RepeatDirection="Horizontal" RepeatColumns="3" runat="server"></asp:CheckBoxList>
+            </div>
+            
+            <div>
+                <h3>Using:</h3>
+                <asp:TextBox ID="TextBoxUsing" TextMode="MultiLine" Width="400" Height="200" runat="server">using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using PageTypeBuilder;
+using EPiServer;
+using EPiServer.Core;</asp:TextBox>
+            </div>
+            <div>
+                <asp:Button ID="ButtonSubmit" Text="Create classes" runat="server" />
+            </div>
+        </asp:PlaceHolder>
+        
+        <asp:Literal ID="LiterlOutput" runat="server"></asp:Literal>
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ CreatePTBClasses/Plugins/Admin/CreatePTBClasses.aspx.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EPiServer;
+using EPiServer.DataAbstraction;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections;
+
+namespace Antecknat.Web.CMS.Plugins.Admin
+{
+    [GuiPlugIn(Area = PlugInArea.AdminMenu, DisplayName = "Create PTB classes from page types", RequiredAccess = EPiServer.Security.AccessLevel.Administer, Url = "~/Plugins/Admin/CreatePTBClasses.aspx")]
+    public partial class CreatePTBClasses : System.Web.UI.Page
+    {
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            ButtonSubmit.Click += new EventHandler(ButtonSubmit_Click);
+        }
+     
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (!IsPostBack)
+            {
+                CheckBoxListPageTypes.DataSource = PageType.List();
+                CheckBoxListPageTypes.DataTextField = "Name";
+                CheckBoxListPageTypes.DataValueField = "ID";
+                CheckBoxListPageTypes.DataBind();
+            }
+
+        }
+        
+        void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            StringBuilder ret = new StringBuilder();
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                PlaceHolderInput.Visible = false;
+                CreatePageTypeFiles(ret);
+                CreateTabs(ret);                
+
+                LiterlOutput.Text = ret.ToString();
+            }
+        }
+
+        StringBuilder CreatePageTypeFiles(StringBuilder ret)
+        {
+            foreach (ListItem li in CheckBoxListPageTypes.Items)
+            {
+                if (!li.Selected)
+                {
+                    PageType pageType = PageType.Load(int.Parse(li.Value));
+                    string dir = OutputDirectory(TextBoxOutput.Text, pageType.Name);
+                    string className = GetClassName(pageType.Name);
+                    string classCont = GetClassContainer(pageType.Name);
+                    ret.AppendFormat("Name: {0}, ClassName: {1}, Directory: {2}<br />", pageType.Name, GetClassName(pageType.Name), dir);
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(OutputDirectory(TextBoxOutput.Text, pageType.Name));
+                    }
+                    using (StreamWriter sw = File.CreateText(dir + "\\" + className + ".cs"))
+                    {
+                        sw.WriteLine(TextBoxUsing.Text);
+                        sw.WriteLine(string.Format(@"using {12}.Tabs;
+
+namespace {0}
+{{
+    [PageType(""{1}"",
+        Name = ""{2}"",
+        Filename = ""{3}"",
+        DefaultChildSortOrder = EPiServer.Filters.FilterSortOrder.{4},
+        Description = ""{5}"",
+        SortOrder = {6},
+        DefaultVisibleInMenu = {7},        
+        AvailablePageTypes = new Type[] {{ {11} }})]
+    
+    public class {8} : {9}
+    {{
+        {10}
+    }}
+}}
+", TextBoxNamespace.Text + (string.IsNullOrEmpty(classCont) ? string.Empty : "." + classCont),
+pageType.GUID,
+pageType.Name,
+pageType.FileName,
+pageType.DefaultChildOrderRule.ToString(),
+pageType.Description.Replace("\"", "'"),
+pageType.SortOrder,
+pageType.DefaultVisibleInMenu.ToString().ToLower(),
+className,
+TextBoxExtend.Text,
+GetProperties(pageType, ret),
+GetAvailablePageTypes(pageType),
+TextBoxNamespace.Text
+));
+                        sw.Flush();
+                    }
+                }
+                else
+                {
+                    ret.AppendFormat("Ignoring {0}<br />", li.Text);
+                }
+            }
+            return ret;
+        }
+
+        private void CreateTabs(StringBuilder ret)
+        {
+            string dir = OutputDirectory(TextBoxOutput.Text, string.Empty);
+            dir += "Tabs";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            foreach (TabDefinition tab in TabDefinition.List())
+            {
+                using (StreamWriter sw = File.CreateText(dir + "/" + ReplaceIllegalChars(tab.Name) + "Tab.cs"))
+                {
+                    sw.WriteLine(string.Format(@"
+using PageTypeBuilder;
+
+namespace {0}.Tabs
+{{
+    public class {1}Tab : Tab
+    {{
+        public override string Name
+        {{
+            get {{ return ""{2}""; }}
+        }}
+
+        public override EPiServer.Security.AccessLevel RequiredAccess
+        {{
+            get {{ return EPiServer.Security.AccessLevel.{3}; }}
+        }}
+
+        public override int SortIndex
+        {{
+            get {{ return {4}; }}
+        }}
+    }}
+}}
+", TextBoxNamespace.Text, ReplaceIllegalChars(tab.Name), tab.Name, tab.RequiredAccess.ToString(), tab.SortIndex ));
+
+                    sw.Flush();
+                }
+            }
+        }
+
+        private object GetProperties(PageType pageType, StringBuilder output)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (PageDefinition pdf in pageType.Definitions)
+            {
+                if (pdf.Name.Contains("-"))
+                {
+                    output.AppendFormat(" Ignoring property: " + pdf.Name);
+                }
+                else
+                {
+                    ret.AppendFormat(@"
+        [PageTypeProperty(EditCaption = ""{0}"",
+            HelpText = ""{1}"",
+            SortOrder = {2},
+            UniqueValuePerLanguage = {3},
+            Required = {4},
+            Searchable = {9},
+            DefaultValue = ""{10}"",
+            DefaultValueType = EPiServer.DataAbstraction.DefaultValueType.{12},
+            Tab = typeof({5}){6}{11})]
+        public virtual {7} {8} {{ get; set; }}
+
+", pdf.EditCaption.Replace("\"", "'"),
+          pdf.HelpText.Replace("\"", "'"),
+          pdf.FieldOrder,
+          pdf.LanguageSpecific.ToString().ToLower(),
+          pdf.Required.ToString().ToLower(),
+          ReplaceIllegalChars(pdf.Tab.Name) + "Tab",
+          string.IsNullOrEmpty(pdf.Type.TypeName) ? "" : string.Format(@",
+            Type = typeof({0})", pdf.Type.TypeName),
+          GetDataType(pdf.Type.DataType),
+          pdf.Name,
+          pdf.Searchable.ToString().ToLower(),
+          pdf.DefaultValue,
+          GetLongStringSettings(pdf),
+          pdf.DefaultValueType.ToString());
+                }
+            }
+
+            return ret.ToString();
+        }
+
+        private string GetLongStringSettings(PageDefinition pdf)
+        {
+            if (pdf.Type.DataType == EPiServer.Core.PropertyDataType.LongString)
+            {
+                if (pdf.LongStringSettings == 0)
+                {
+                    return @", 
+            ClearAllLongStringSettings = true";
+                }
+                else
+                {                    
+                    StringBuilder ret = new StringBuilder();
+                    ret.AppendFormat(@",
+            LongStringSettings = EPiServer.Editor.EditorToolOption.All");
+                    foreach (EPiServer.Editor.EditorToolOption a in Enum.GetValues(typeof(EPiServer.Editor.EditorToolOption)))
+                    {
+                        if ((pdf.LongStringSettings & a) <= 0 && a != EPiServer.Editor.EditorToolOption.All)
+                        {
+                            ret.AppendFormat(" ^ {0}", "EPiServer.Editor.EditorToolOption." + a.ToString());
+                        }
+                    }
+
+                    return ret.ToString();
+                }
+            }
+            return string.Empty;
+        }
+
+        private string GetDataType(EPiServer.Core.PropertyDataType propertyDataType)
+        {
+            switch (propertyDataType)
+            {
+                case EPiServer.Core.PropertyDataType.Boolean:
+                    return "bool";
+                case EPiServer.Core.PropertyDataType.Category:
+                    return "CategoryCollection";
+                case EPiServer.Core.PropertyDataType.Date:
+                    return "DateTime";
+                case EPiServer.Core.PropertyDataType.FloatNumber:
+                    return "double";
+                case EPiServer.Core.PropertyDataType.LinkCollection:
+                    return "LinkItemCollection";
+                case EPiServer.Core.PropertyDataType.LongString:
+                    return "string";
+                case EPiServer.Core.PropertyDataType.Number:
+                    return "int";
+                case EPiServer.Core.PropertyDataType.PageReference:
+                    return "PageReference";
+                case EPiServer.Core.PropertyDataType.PageType:
+                    return "int";
+                case EPiServer.Core.PropertyDataType.String:
+                    return "string";
+                default:
+                    return "object";
+            }
+        }
+
+        string OutputDirectory(string output, string name)
+        {
+            return Server.MapPath("/" + TextBoxOutput.Text.Trim() + "/" + GetClassContainer(name));
+        }
+
+        string GetClassContainer(string name)
+        {
+            var pattern = @"\[(.*?)\]";
+            var match = Regex.Match(name, pattern);
+            return match.Value.TrimStart('[').TrimEnd(']');
+        }
+        
+        string GetClassName(string name)
+        {
+            var pattern = @"\[(.*?)\]";
+            name = Regex.Replace(name, pattern, "");
+            name = ReplaceIllegalChars(name);
+            return TextBoxPrefixBefore.Text.Trim() + name + TextBoxPrefixAfter.Text.Trim();
+        }
+
+        string ReplaceIllegalChars(string inputString)
+        {            
+            Regex regexFindInvalidUrlChars = new Regex(@"[^A-Za-z0-9\-_~]{1}", RegexOptions.Compiled);
+            Hashtable _urlCharacterMap = EPiServer.Web.UrlSegment.GetURLCharacterMap();
+
+            StringBuilder builder = new StringBuilder(inputString);
+            MatchCollection matchs = regexFindInvalidUrlChars.Matches(inputString);
+            for (int i = 0; i < matchs.Count; i++)
+            {
+                object obj2 = _urlCharacterMap[builder[matchs[i].Index]];
+                if (obj2 != null)
+                {
+                    builder[matchs[i].Index] = (char)obj2;
+                }
+                else
+                {
+                    builder[matchs[i].Index] = '?';
+                }
+            }
+            builder.Replace("?", "");
+            builder.Replace(" ", "");
+            builder.Replace("-", "");
+            return builder.ToString();
+        }
+
+        private string GetAvailablePageTypes(PageType pageType)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (string s in pageType.AllowedPageTypeNames)
+            {
+                string cont = GetClassContainer(s);
+                string typeName = string.IsNullOrEmpty(cont) ? GetClassName(s) : cont + "." + GetClassName(s);
+                ret.AppendFormat(@" typeof({0}), ", typeName);
+            }
+            if (ret.Length > 0) {
+                return ret.ToString().Trim().TrimEnd(',');
+            }
+            return ret.ToString();
+        }
+        
+    }
+}
